66018cccd9bd54a18424d596d12986823e9ad424
Yaroslav Lelkin <ilelkin@icloud.com>
2026-04-11 12:32:50 +0500

    fix memory management and clean up code

--- wlcapture.c
+++ wlcapture.c
@@ -1,4 +1,3 @@
-#include <libavutil/pixfmt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -23,10 +22,6 @@
 #include <libavutil/imgutils.h>
 #include <libswscale/swscale.h>
 
-thrd_t CURR;
-
-static uint8_t is_curr(void) { return thrd_equal(CURR, thrd_current()); }
-
 const struct wl_interface *capture_interface =
     &ext_output_image_capture_source_manager_v1_interface;
 const struct wl_interface *copy_interface =
@@ -37,9 +32,9 @@ typedef struct ext_output_image_capture_source_manager_v1 *capture_manager_t;
 typedef struct ext_image_copy_capture_manager_v1 *copy_manager_t;
 typedef struct ext_image_copy_capture_session_v1 *capture_session_t;
 typedef struct ext_image_copy_capture_frame_v1 *capture_frame_t;
-#define capture_manager_create_source                                          \
+#define capture_manager_create_source \
   ext_output_image_capture_source_manager_v1_create_source
-#define copy_manager_create_session                                            \
+#define copy_manager_create_session \
   ext_image_copy_capture_manager_v1_create_session
 #define copy_session_add_listener ext_image_copy_capture_session_v1_add_listener
 
@@ -52,8 +47,10 @@ struct app_state {
   struct wl_shm *shm;
 };
 
-static void registry_handle_global(void *data, struct wl_registry *registry,
-                                   uint32_t name, const char *interface,
+static void registry_handle_global(void *data,
+                                   struct wl_registry *registry,
+                                   uint32_t name,
+                                   const char *interface,
                                    uint32_t version) {
   struct app_state *state = data;
   if (!strcmp(interface, capture_interface->name)) {
@@ -96,20 +93,24 @@ struct capture_state {
   uint8_t done;
 };
 
-static void capture_set_buffer_size(void *data, capture_session_t session,
-                                    uint32_t width, uint32_t height) {
+static void capture_set_buffer_size(void *data,
+                                    capture_session_t session,
+                                    uint32_t width,
+                                    uint32_t height) {
   struct capture_state *state = data;
   state->width = width;
   state->height = height;
 }
 
-static void capture_set_shm_format(void *data, capture_session_t session,
+static void capture_set_shm_format(void *data,
+                                   capture_session_t session,
                                    enum wl_shm_format format) {
   struct capture_state *state = data;
   state->shm_format = format;
 }
 
-static void capture_set_dmabuf_format(void *data, capture_session_t session,
+static void capture_set_dmabuf_format(void *data,
+                                      capture_session_t session,
                                       uint32_t format,
                                       struct wl_array *modifiers) {
   struct capture_state *state = data;
@@ -122,13 +123,15 @@ static void capture_set_dmabuf_format(void *data, capture_session_t session,
   }
 }
 
-static void capture_set_dmabuf_device(void *data, capture_session_t session,
+static void capture_set_dmabuf_device(void *data,
+                                      capture_session_t session,
                                       struct wl_array *devices) {
   struct capture_state *state = data;
   state->devices_cnt = 0;
-  dev_t *cur;
+  dev_t *cur, *elem;
   wl_array_for_each(cur, devices) {
-    state->devices[state->devices_cnt++] = *cur;
+    elem = &state->devices[state->devices_cnt++];
+    memcpy(elem, cur, sizeof(dev_t));
   }
 }
 
@@ -153,14 +156,21 @@ struct frame_data {
   uint32_t failed_reason;
 };
 
-static void frame_transform(void *data, capture_frame_t frame,
+static void frame_transform(void *data,
+                            capture_frame_t frame,
                             uint32_t transform) {}
 
-static void frame_damage(void *data, capture_frame_t frame, int32_t x,
-                         int32_t y, int32_t width, int32_t height) {}
+static void frame_damage(void *data,
+                         capture_frame_t frame,
+                         int32_t x,
+                         int32_t y,
+                         int32_t width,
+                         int32_t height) {}
 
-static void frame_presentation_time(void *data, capture_frame_t frame,
-                                    uint32_t tv_sec_hi, uint32_t tv_sec_lo,
+static void frame_presentation_time(void *data,
+                                    capture_frame_t frame,
+                                    uint32_t tv_sec_hi,
+                                    uint32_t tv_sec_lo,
                                     uint32_t tv_nsec) {
   struct frame_data *state = data;
   state->tv_sec_hi = tv_sec_hi;
@@ -189,16 +199,16 @@ static const struct ext_image_copy_capture_frame_v1_listener frame_listener = {
 
 static enum AVPixelFormat get_av_format(uint32_t wl_shm_format) {
   switch (wl_shm_format) {
-  case WL_SHM_FORMAT_XRGB8888:
-    return AV_PIX_FMT_BGR0;
-  case WL_SHM_FORMAT_ARGB8888:
-    return AV_PIX_FMT_BGRA;
-  case WL_SHM_FORMAT_XBGR8888:
-    return AV_PIX_FMT_RGB0;
-  case WL_SHM_FORMAT_ABGR8888:
-    return AV_PIX_FMT_RGBA;
-  default:
-    return AV_PIX_FMT_NB;
+    case WL_SHM_FORMAT_XRGB8888:
+      return AV_PIX_FMT_BGR0;
+    case WL_SHM_FORMAT_ARGB8888:
+      return AV_PIX_FMT_BGRA;
+    case WL_SHM_FORMAT_XBGR8888:
+      return AV_PIX_FMT_RGB0;
+    case WL_SHM_FORMAT_ABGR8888:
+      return AV_PIX_FMT_RGBA;
+    default:
+      return AV_PIX_FMT_NB;
   }
 }
 
@@ -278,9 +288,35 @@ static enum AVPixelFormat av_get_desired_format(const AVCodec *codec,
                                            &losses);
 }
 
+static void app_state_free(struct app_state *state) {
+  wl_shm_destroy(state->shm);
+  ext_image_copy_capture_manager_v1_destroy(state->copy_manager);
+  ext_output_image_capture_source_manager_v1_destroy(state->capture_manager);
+  wl_output_destroy(state->output);
+  wl_registry_destroy(state->registry);
+  wl_display_disconnect(state->display);
+}
+
+static void capture_src_free(struct ext_image_capture_source_v1 **src) {
+  ext_image_capture_source_v1_destroy(*src);
+}
+
+static void capture_session_free(
+    struct ext_image_copy_capture_session_v1 **sess) {
+  ext_image_copy_capture_session_v1_destroy(*sess);
+}
+
+static void shm_buf_free(struct shm_buf *shm_buf) {
+  wl_buffer_destroy(shm_buf->buf);
+  munmap(shm_buf->mapping, shm_buf->size);
+}
+
+static void frame_free(struct ext_image_copy_capture_frame_v1 **frame) {
+  ext_image_copy_capture_frame_v1_destroy(*frame);
+}
+
 int main(int argc, char *argv[]) {
-  CURR = thrd_current();
-  struct app_state wl_state = {0};
+  struct app_state wl_state __attribute__((cleanup(app_state_free))) = {0};
 
   wl_state.display = wl_display_connect(NULL);
   if (wl_state.display == NULL) {
@@ -293,11 +329,14 @@ int main(int argc, char *argv[]) {
   wl_registry_add_listener(wl_state.registry, &registry_listener, &wl_state);
   wl_display_roundtrip(wl_state.display);
 
-  struct ext_image_capture_source_v1 *src =
-      capture_manager_create_source(wl_state.capture_manager, wl_state.output);
+  struct ext_image_capture_source_v1 *src
+      __attribute__((cleanup(capture_src_free))) =
+          capture_manager_create_source(wl_state.capture_manager,
+                                        wl_state.output);
 
-  struct ext_image_copy_capture_session_v1 *session =
-      copy_manager_create_session(wl_state.copy_manager, src, 0);
+  struct ext_image_copy_capture_session_v1 *session
+      __attribute__((cleanup(capture_session_free))) =
+          copy_manager_create_session(wl_state.copy_manager, src, 0);
 
   struct capture_state cp_state = {0};
   copy_session_add_listener(session, &listener, &cp_state);
@@ -305,7 +344,7 @@ int main(int argc, char *argv[]) {
   while (!cp_state.done) {
     wl_display_dispatch(wl_state.display);
   }
-  struct shm_buf shm_buf = {0};
+  struct shm_buf shm_buf __attribute__((cleanup(shm_buf_free))) = {0};
   shm_buf_error ret = shm_buf_create(wl_state.shm, &cp_state, &shm_buf);
   if (ret != SHM_BUF_OK) {
     return ret;
@@ -313,8 +352,9 @@ int main(int argc, char *argv[]) {
 
   // according to protocol, we must create frame, attach buffer, then damage
   // this buffer
-  struct ext_image_copy_capture_frame_v1 *frame =
-      ext_image_copy_capture_session_v1_create_frame(session);
+  struct ext_image_copy_capture_frame_v1 *frame
+      __attribute__((cleanup(frame_free))) =
+          ext_image_copy_capture_session_v1_create_frame(session);
 
   ext_image_copy_capture_frame_v1_attach_buffer(frame, shm_buf.buf);
 
@@ -334,17 +374,16 @@ int main(int argc, char *argv[]) {
     puts("JXL not found");
     return EXIT_FAILURE;
   }
-  AVDictionary *libjxl_opts = NULL;
+  AVDictionary *libjxl_opts __attribute__((cleanup(av_dict_free))) = NULL;
 
-  // av_dict_set(&libjxl_opts, "distance", "1.0", 0);
-  // av_dict_set(&libjxl_opts, "effort", "9", 0);
-  // av_dict_set(&libjxl_opts, "modular", "1", 0);
-  // av_dict_set(&libjxl_opts, "xyb", "0", 0);
+  // lossless JXL
+  av_dict_set(&libjxl_opts, "distance", "0.0", 0);
 
   const enum AVPixelFormat av_tgt_fmt =
       av_get_desired_format(codec, shm_buf.pixel_format);
 
-  AVCodecContext *ctx = avcodec_alloc_context3(codec);
+  AVCodecContext *ctx __attribute__((cleanup(avcodec_free_context))) =
+      avcodec_alloc_context3(codec);
   ctx->width = cp_state.width;
   ctx->height = cp_state.height;
   ctx->pix_fmt = av_tgt_fmt;
@@ -352,7 +391,7 @@ int main(int argc, char *argv[]) {
   ctx->time_base = time_base;
   ctx->color_range = AVCOL_RANGE_JPEG;
   ctx->color_primaries = AVCOL_PRI_BT709;
-  ctx->color_trc = AVCOL_TRC_IEC61966_2_1;
+  ctx->color_trc = AVCOL_TRC_BT709;
   ctx->colorspace = AVCOL_SPC_RGB;
   ret = avcodec_open2(ctx, codec, &libjxl_opts);
   if (ret < 0) {
@@ -360,43 +399,45 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  AVFrame *src_frame = av_frame_alloc();
-  src_frame->width = cp_state.width;
-  src_frame->height = cp_state.height;
-  src_frame->format = shm_buf.pixel_format;
-  av_image_fill_arrays(src_frame->data, src_frame->linesize, shm_buf.mapping,
+  AVFrame *src_fr __attribute__((cleanup(av_frame_free))) = av_frame_alloc();
+  src_fr->width = cp_state.width;
+  src_fr->height = cp_state.height;
+  src_fr->format = shm_buf.pixel_format;
+  av_image_fill_arrays(src_fr->data, src_fr->linesize, shm_buf.mapping,
                        shm_buf.pixel_format, cp_state.width, cp_state.height,
                        4);
 
-  AVFrame *final_frame = src_frame;
+  AVFrame *dst_fr __attribute__((cleanup(av_frame_free))) = av_frame_alloc();
 
   SwsContext *sw_scale_ctx = NULL;
   if (shm_buf.pixel_format != av_tgt_fmt) {
-    final_frame = av_frame_alloc();
-    final_frame->width = cp_state.width;
-    final_frame->height = cp_state.height;
-    final_frame->format = av_tgt_fmt;
-    av_frame_get_buffer(final_frame, 0);
+    dst_fr->width = cp_state.width;
+    dst_fr->height = cp_state.height;
+    dst_fr->format = av_tgt_fmt;
+    av_frame_get_buffer(dst_fr, 0);
 
     sw_scale_ctx = sws_getContext(
         cp_state.width, cp_state.height, shm_buf.pixel_format, cp_state.width,
         cp_state.height, av_tgt_fmt, SWS_BILINEAR, NULL, NULL, NULL);
 
-    sws_scale(sw_scale_ctx, (const uint8_t *const *)src_frame->data,
-              src_frame->linesize, 0, cp_state.height, final_frame->data,
-              final_frame->linesize);
+    sws_scale(sw_scale_ctx, (const uint8_t *const *)src_fr->data,
+              src_fr->linesize, 0, cp_state.height, dst_fr->data,
+              dst_fr->linesize);
+    sws_freeContext(sw_scale_ctx);
+  } else {
+    av_frame_move_ref(dst_fr, src_fr);
   }
 
-  final_frame->color_range = ctx->color_range;
-  final_frame->color_primaries = ctx->color_primaries;
-  final_frame->color_trc = ctx->color_trc;
-  final_frame->colorspace = ctx->colorspace;
+  dst_fr->color_range = ctx->color_range;
+  dst_fr->color_primaries = ctx->color_primaries;
+  dst_fr->color_trc = ctx->color_trc;
+  dst_fr->colorspace = ctx->colorspace;
 
-  AVPacket *pkt = av_packet_alloc();
+  AVPacket *pkt __attribute__((cleanup(av_packet_free))) = av_packet_alloc();
   if (!pkt) {
     return EXIT_FAILURE;
   }
-  ret = avcodec_send_frame(ctx, final_frame);
+  ret = avcodec_send_frame(ctx, dst_fr);
   if (ret) {
     return EXIT_FAILURE;
   }
@@ -408,7 +449,4 @@ int main(int argc, char *argv[]) {
     }
     av_packet_unref(pkt);
   }
-
-  munmap(shm_buf.mapping, shm_buf.size);
-  avcodec_free_context(&ctx);
 }
